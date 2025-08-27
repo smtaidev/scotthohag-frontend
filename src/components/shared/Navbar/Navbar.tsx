f7e06ce6f5e679169383430635847d9e29c5c00d
Apu Roy <apuroy2785@gmail.com>
2025-08-27 14:41:28 +0600

    added the profile and logout

--- src/components/shared/Navbar/Navbar.tsx
+++ src/components/shared/Navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import { FaUser, FaTimes, FaBars } from 'react-icons/fa';
 import { motion, AnimatePresence } from 'framer-motion';
 import Logo from '../Logo';
 import { useGetMeQuery } from '@/redux/api/getMe/getMeApi';
+import { div } from 'framer-motion/client';
 
 const navItems = [
     { name: 'Home', link: '/' },
@@ -15,11 +16,19 @@ const navItems = [
     { name: 'Contact', link: '/contact' },
 ];
 
+const profile = [
+    { name: 'Profile', link: '/' },
+    { name: 'Log Out', link: '/' },
+
+];
+
 export default function Navbar() {
     const [menuOpen, setMenuOpen] = useState(false);
+    const [profileOpen, setProfileOpen] = useState(false);
     const [activeItem, setActiveItem] = useState("/");
     const [isScrolled, setIsScrolled] = useState(false);
     const main = useRef<HTMLDivElement>(null);
+    const prof=useRef<HTMLDivElement>(null);
 
 
     const { data: userInfo } = useGetMeQuery({});
@@ -36,12 +45,22 @@ export default function Navbar() {
         const handleClickOutside = (event: MouseEvent) => {
             if (main.current && !main.current.contains(event.target as Node)) {
                 setMenuOpen(false);
+                setMenuOpen(false);
+            }
+        };
+
+        const handleClickOutside2 = (event: MouseEvent) => {
+            if (prof.current && !prof.current.contains(event.target as Node)) {
+           
+                setProfileOpen(false);
             }
         };
 
         document.addEventListener("mousedown", handleClickOutside);
+        document.addEventListener("mousedown", handleClickOutside2);
         return () => {
             document.removeEventListener("mousedown", handleClickOutside);
+            document.removeEventListener("mousedown", handleClickOutside2);
         };
     }, []);
 
@@ -88,9 +107,34 @@ export default function Navbar() {
                     {!userInfo?.data ?
                         <Link href={"/signIn"} className="bg-secondary hover:bg-green-600 text-white px-4 py-2 rounded-md text-xs md:text-lg font-semibold transition-colors cursor-pointer">
                             Sign Up
-                        </Link> : <div className='cursor-pointer hover:bg-gray-200/20 p-2 transition rounded-full '>
-                            <FaUser className='size-6 ' />
-                        </div>}
+                        </Link> :
+                        <div className='relative'>
+                            <div  onClick={() => setProfileOpen(!profileOpen)} ref={prof} className='cursor-pointer hover:bg-gray-200/20 p-2 transition rounded-full '>
+                                <FaUser className='size-6 ' />
+                            </div>
+                            <div
+                                className={`absolute right-0 top-12 min-w-[160px] bg-white/50 backdrop-blur-2xl text-black shadow-lg rounded-md transition-all duration-300 transform z-50 ${profileOpen ? "opacity-100 scale-100" : "opacity-0 scale-95 pointer-events-none"
+                                    }`}
+                            >
+                                <div className="flex flex-col p-3 divide-y">
+                                    {profile?.map((item) => (
+                                        <Link
+                                            key={item.name}
+                                            href={item.link}
+                                            onClick={() => {
+                                                handleItemClick(item.link);
+                                                // setDropdownOpen(false); // Close dropdown on link click
+                                            }}
+                                            className={`transition-all duration-200 py-2 px-3  hover:bg-gray-100/20  ${activeItem === item.link ? "text-secondary font-semibold" : ""
+                                                }`}
+                                        >
+                                            {item.name}
+                                        </Link>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    }
                     <button
                         onClick={() => setMenuOpen(!menuOpen)}
                         className="lg:hidden text-xl p-2"
@@ -107,7 +151,7 @@ export default function Navbar() {
             <AnimatePresence>
                 {menuOpen && (
                     <motion.div
-                        className="absolute top-full left-0 w-full bg-[#0A2540] text-white flex flex-col items-center py-4 lg:hidden z-50 shadow-lg"
+                        className="absolute top-full left-0 w-full bg-[#0A2540] text-white flex flex-col items-center py-4 lg:hidden z-30 shadow-lg"
                         initial={{ opacity: 0, height: 0 }}
                         animate={{ opacity: 1, height: "auto" }}
                         exit={{ opacity: 0, height: 0 }}
